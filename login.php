d82b45c05563e6fd466193a0ce83325f614edf9f
Kluczleks <kluczleks.twitch@gmail.com>
2024-05-20 09:28:18 +0200

    20.05

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
 //zaimportuj kod klasy
 require_once('class/User.class.php');
+//inicjalizacja sesji
+session_start();
+
 
 if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
     //wysłano formularz - przechwyć i obrób dane
@@ -9,6 +12,8 @@ if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
     //wywołujemy metodę klasy
     //metody statyczne w PHP wywołuje się poprzez ::
     $result = User::Login($email, $password);
+    //$result zawiera true jeśli udało się zalogować lub 
+    //fasle w innym wypadku
 }
 ?>
 <!DOCTYPE html>
@@ -26,7 +31,7 @@ if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
     <div id="container">
         <h1 class="text-center mt-5 mb-5">Zaloguj się:</h1>
         <div id="loginForm" class="col-4 offset-4 mt-5">
-            <form action="register.php" method="post">
+            <form action="login.php" method="post">
                 <label for="emailInput" class="form-label">Adres e-mail:</label>
                 <input type="email" class="form-control mb-3" name="email" id="emailInput">
 
@@ -34,19 +39,29 @@ if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
                 <input type="password" class="form-control mb-3" name="password" id="passwordInput">
 
                 <button type="submit" class="btn btn-primary w-100 mt-3">Zaloguj</button>
+                
             </form>
+                <a href="index.php">
+                <button class="btn btn-primary w-100 mt-3">Powrót</button>
+                </a>
             <?php
             if (isset($result)) {
                 if ($result) {
-                    echo "Udało się zalogować konto";
+                    echo "Użytkownik zalogowany<br>";
+                    echo '<a href="index.php">Przejdź do głównej strony</a>';
+                    
                 } else {
-                    echo "Nie udało się zalogować konta";
+                    echo "Użytkownik nie zalogowany";
                 }
             }
 
             ?>
         </div>
     </div>
+    <?php
+    echo "<pre>";
+   
+    ?>
 </body>
 
 </html>
